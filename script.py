c2bac5feb2b334175aa862e29915ceea6be80515
Bhavesh <mannusaini2005@gmail.com>
2024-01-20 12:57:25 -0500

    added config

--- script.py
+++ script.py
@@ -2,18 +2,20 @@ import sys
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import smtplib
+from config import * 
 
 def sendMail(): # IMPORT SAMBA INFO
     if len(sys.argv) > 1:
         print(sys.argv[1])
         print(sys.argv[2])
         print(sys.argv[3])
+
         if "New session" in sys.argv[1] or "Failed password" in sys.argv[1]:
 
 
             # Email configuration
-            sender_email = ""
-            receiver_email = ""
+            sender_email = sender
+            receiver_email = receiver
             subject = "ALERT"
             body = sys.argv[1] + "\n" + sys.argv[2]
 
@@ -29,8 +31,8 @@ def sendMail(): # IMPORT SAMBA INFO
             # SMTP server configuration (for Gmail)
             smtp_server = "smtp.gmail.com"
             smtp_port = 587
-            smtp_username = ""
-            smtp_password = ""
+            smtp_username = username
+            smtp_password = password
 
             # Create an SMTP session
             with smtplib.SMTP(smtp_server, smtp_port) as server:
